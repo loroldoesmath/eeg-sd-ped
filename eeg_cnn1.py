e73d75bd0eae2ba58579eaa8df5b18167d116659
Laurel Weber <M02520@ad.mtec.edu>
2024-09-11 09:21:29 -0600

    Initial commit

---
+++ eeg_cnn1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import requests
+from sklearn.preprocessing import StandardScaler
+
+
+# Load data - q: is streaming the best way?
+url = 'https://archive.physionet.org/physiobank/database/chbmit/chb01/chb01_04.edf'
+response = requests.get(url, stream = True)
+
+with open ('data_chunk.edf', 'wb') as file:
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            file.write(chunk)
+
+# Handle missing values?
+
+# Normalize/standardize data
+
+# scaler = StandardScaler()
+# scaled_data = scalar.fit_transform(data)
